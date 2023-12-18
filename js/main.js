6db6b301908c723697720ecdfc44db057cae29ad
jerrysheh <jerrysheh@gmail.com>
2023-12-18 21:53:44 +0800

    Site updated: 2023-12-18 21:53:40

--- js/main.js
+++ js/main.js
@@ -20,11 +20,11 @@
         $('.justified-gallery').justifiedGallery();
     }
 
-    // if (typeof moment === 'function') {
-    //     $('.article-meta time').each(function() {
-    //         $(this).text(moment($(this).attr('datetime')).fromNow());
-    //     });
-    // }
+    if (typeof moment === 'function') {
+        $('.article-meta time').each(function() {
+            $(this).text(moment($(this).attr('datetime')).fromNow());
+        });
+    }
 
     $('.article > .content > table').each(function() {
         if ($(this).width() > $(this).parent().width()) {
@@ -98,6 +98,8 @@
 
         if (fold) {
             $('figure.highlight').each(function() {
+                $(this).addClass('foldable'); // add 'foldable' class as long as fold is enabled
+
                 if ($(this).find('figcaption').find('span').length > 0) {
                     const span = $(this).find('figcaption').find('span');
                     if (span[0].innerText.indexOf('>folded') > -1) {
@@ -111,7 +113,7 @@
                 toggleFold(this, fold === 'folded');
             });
 
-            $('figure.highlight figcaption .fold').click(function() {
+            $('figure.highlight figcaption .level-left').click(function() {
                 const $code = $(this).closest('figure.highlight');
                 toggleFold($code.eq(0), !$code.hasClass('folded'));
             });
