984ae3e09673fde0c097545da76c6286ac687124
jerrysheh <jerrysheh@gmail.com>
2026-01-19 16:03:21 +0800

    typo

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -45,7 +45,7 @@ export const navbar = defineNavbarConfig([
   },
   { text: '工具',
     items: [
-      { text: '字幕拼接大师', link: '/webapp/SubtitleJoiner' },
+      { text: '字幕拼图大师', link: '/webapp/SubtitleJoiner' },
     ] 
   },  { text: '关于', link: '/about/' },
 
